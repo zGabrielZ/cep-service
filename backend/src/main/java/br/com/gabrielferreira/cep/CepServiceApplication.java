2ff238bda4994f4c292352201afe49fd10374738
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2026-04-14 19:58:58 -0300

    first commit

---
+++ backend/src/main/java/br/com/gabrielferreira/cep/CepServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrielferreira.cep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepServiceApplication.class, args);
+	}
+
+}
